6ba9e64bff373a38608e6fe403ce656a8a0276c2
ElenaLj <elenaljubijankic@gmail.com>
2021-11-25 15:19:32 +0100

    prima prova vue

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // alert("Hello World!");
+
+//prove con Vue seguendo il video della lezione
+const app = new Vue({
+    el: "#root",
+    data: {
+       messaggio: "Ciao mondo!" 
+    }
+});
+
+//il vantaggio di Vue è che tiene costantemente monitorato il valore di una variabile anche se l'utente la modifica;
